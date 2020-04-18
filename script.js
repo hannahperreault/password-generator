2fe430ba908346f20f1f83bf631cf516e9bc645a
Hannah Perreault <hperre@gmail.com>
2020-04-18 15:44:51 -0400

    added console log of all array combined, random generator not working

--- script.js
+++ script.js
@@ -13,13 +13,28 @@ var special = ["~", "!", "@", "#", "$", "&", "^", "*", "(", ")", "-", "_"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 
-// Write password to the #password input
+function createLibrary() {
+    if (specialCharacters && wantUpperCase && wantNumbers);
+    var totalArray = [lowercase + special + uppercase + numbers];
+    var randomPassword = Math.floor(Math.random() * totalArray.length);
 
-function writePassword() {
-
+    console.log(totalArray[randomPassword]);
 }
 
-writePassword();
+createLibrary();
+
+
+
+
+
+
+// Write password to the #password input
+
+//function writePassword() {
+
+//}
+
+// writePassword();
 
 
 
