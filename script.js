9bfa902f866ce9bf4d7a1301a81e40596dfc540c
Hannah Perreault <hperre@gmail.com>
2020-04-18 14:31:24 -0400

    created arrays of character options

--- script.js
+++ script.js
@@ -7,14 +7,35 @@ var wantNumbers = confirm("Do you want numbers?");
 
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var special = ["~", "!", "@", "#", "$", "&", "^", "*", "(", ")", "-", "_"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-    passwordText.value = password;
+
+// Write password to the #password input
+
+function writePassword() {
 
 }
 
+writePassword();
+
+
+
+// function writePassword() {
+//     var length = howMany;
+//    var characters = "abcdefghijklmnop";
+``
+// }
+
+// var password = generatePassword();
+// var passwordText = document.querySelector("#password");
+
+// passwordText.value = password;
+
+// }
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
+
