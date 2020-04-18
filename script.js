0452f042dfee10c5fb40a4131039e22c4de8cb26
Hannah Perreault <hperre@gmail.com>
2020-04-18 12:04:53 -0400

    added prompts & confirms

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 // Assignment Code
+var howMany = prompt("How many characters do you want?");
+var specialCharacters = confirm("Do you want special characters?");
+var wantUpperCase = confirm("Do you want uppercase letters?");
+var wantNumbers = confirm("Do you want numbers?");
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
